5bc55186fe38f0b3a286ea51d029e8080f68c64e
Boygioi15 <boygioi85@gmail.com>
2024-11-19 06:34:35 +0700

    commit 4

--- tests/src/copyLater.js
+++ tests/src/copyLater.js
@@ -1,3 +1,38 @@
+const { Builder, By } = require("selenium-webdriver");
+
+(async function testFormValidation() {
+  //config browser with chrome.
+  const driver = await new Builder().forBrowser("chrome").build();
+
+  try {
+    //point to the url: http://localhost:5173
+    await driver.get("http://localhost:5173");
+
+    await driver.findElement(By.id("nameInput")).sendKeys("Hello World");
+    await driver.sleep(1000);
+    await driver.findElement(By.id("ageInput")).sendKeys("49");
+    await driver.sleep(1000);
+    await driver.findElement(By.css("button[type='submit']")).click();
+
+    // Verify validation message
+    let validationText = await driver
+      .findElement(By.id("validationMessage"))
+      .getText();
+    if (validationText === "") {
+      console.log("Test 1 Passed");
+    } else {
+      console.log("Test 1 Failed. Message: ${validationText}");
+    }
+
+    // Clear inputs before next test
+  } catch (error) {
+    console.error("An error occurred:", error);
+  } finally {
+    await driver.sleep(2000);
+    await driver.quit();
+  }
+})();
+
 // Test 2: Valid submission
 await driver.findElement(By.id("nameInput")).sendKeys("Jo");
 await driver.findElement(By.id("emailInput")).sendKeys("test@example.com");
--- tests/src/source.js
+++ tests/src/source.js
@@ -1,35 +1,7 @@
 const { Builder, By } = require("selenium-webdriver");
 
-(async function testFormValidation() {
-  //config browser with chrome.
-  try {
-    const driver = await new Builder().forBrowser("chrome").build();
-    //point to the url: http://localhost:5173
-    await driver.get("http://localhost:5173");
-
-    await driver.findElement(By.id("nameInput")).sendKeys("Hello World");
-    await driver.sleep(1000);
-    await driver.findElement(By.id("ageInput")).sendKeys("49");
-    await driver.sleep(1000);
-    await driver.findElement(By.css("button[type='submit']")).click();
-
-    // Verify validation message
-    let validationText = await driver
-      .findElement(By.id("validationMessage"))
-      .getText();
-    if (validationText === "") {
-      console.log("Test 1 Passed");
-    } else {
-      console.log("Test 1 Failed. Message: ${validationText}");
-    }
-
-    // Clear inputs before next test
-    await driver.findElement(By.id("nameInput")).clear();
-    await driver.findElement(By.id("ageInput")).clear();
-  } catch (error) {
-    console.error("An error occurred:", error);
-  } finally {
-    await driver.sleep(2000);
-    await driver.quit();
-  }
-})();
+async function test() {
+  const driver = await new Builder().forBrowser("chrome").build();
+  await driver.get("http://localhost:5173");
+}
+test();
